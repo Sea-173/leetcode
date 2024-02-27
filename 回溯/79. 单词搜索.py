66e3464dd046f200da49cadc3287faa25d939da4
Sea-173 <2534062529@qq.com>
2024-02-27 17:46:21 +0800

    0227 79

--- 回溯/79. 单词搜索.py
+++ 回溯/79. 单词搜索.py
@@ -6,3 +6,26 @@
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        def dfs(i, j, index):
+            if index == len(word):
+                return True
+            for x, y in ([(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]):
+                if x< 0 or x >= len(board) or y < 0 or y >= len(board[0]):
+                    continue
+
+                if board[x][y] == word[index]:
+                    print(x, y)
+                    board[x][y] = '#'
+                    if dfs(x, y, index + 1):
+                        return True
+                    board[x][y] = word[index]
+            return False
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == word[0]:
+                    board[i][j] = '#'
+                    if dfs(i, j, 1):
+                        return True
+                    board[i][j] = word[0]
+        return False
