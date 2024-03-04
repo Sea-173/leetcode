0bdbc2c01ee07acde2d5b286485094c3f3bba0ea
Sea-173 <2534062529@qq.com>
2024-03-04 10:37:48 +0800

    0304

--- 栈/394. 字符串解码.py
+++ 栈/394. 字符串解码.py
@@ -7,3 +7,40 @@
 
 此外，你可以认为原始数据不包含数字，所有的数字只表示重复的次数 k ，例如不会出现像 3a 或 2[4] 的输入。
 """
+
+class Solution:
+    def decodeString(self, s: str) -> str:
+        result = ""
+        stack = 0
+        num = 0
+        len_s = len(s)
+
+        i = 0
+        while i < len_s:
+            if s[i].isnumeric():
+                # 遇到数字，将其与后续连续的数字拼接起来
+                for j in range(i, len_s):
+                    if s[j].isnumeric():
+                        num = num * 10 + int(s[j])
+                    else:
+                        break
+                for k in range(j, len_s):
+                    if s[k] == '[':
+                        stack += 1
+                    elif s[k] == ']':
+                        stack -= 1
+                        if stack == 0:
+                            sub_s = self.decodeString(s[j+1:k])
+                            result += sub_s * num
+                            num = 0
+                            break
+                i = k + 1
+            elif s[i].isalpha():
+                result += s[i]
+                i += 1
+        return result
+
+# s1 = Solution()ß
+# s = "3[a]2[bc]"
+# print(s1.decodeString(s))
+                               
--- 链表/148. 排序链表.py
+++ 链表/148. 排序链表.py
@@ -12,79 +12,66 @@
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
-        def mergeSort(left: ListNode, right: ListNode) -> ListNode:
-            '''合并两个已排序好的链表 left 和 right'''
-            dummy_head = ListNode(-1)   # 虚拟节点
-            tmp = dummy_head
-            while left and right:       # left 和 right 均未遍历完
-                if left.val <= right.val:
-                    tmp.next = left
+        def mergeSort(left, right):
+            dummyHead = ListNode(-1)
+            cur = dummyHead
+
+            while left and right:
+                if left.val < right.val:
+                    cur.next = left
                     left = left.next
                 else:
-                    tmp.next = right
+                    cur.next = right
                     right = right.next
-                tmp = tmp.next
-            if left:                    # left 未遍历完
-                tmp.next = left
-            if right:                   # right 未遍历完
-                tmp.next = right
-            return dummy_head.next
+                cur = cur.next
+            if left:
+                cur.next = left
+            elif right:
+                cur.next = right
+            return dummyHead.next
         
-
-        '''主程序开始'''
-        n = 0               # 计算链表总长度
-        tmp = head
-        while tmp:
-            tmp = tmp.next
+        n = 0
+        cur = head
+        while cur:
             n += 1
+            cur = cur.next
         
-        dummy_head = ListNode(-1, head)     # 虚拟节点，并指向head
-
-        k = 1        # 子链表长度从 k=1 开始，逐步加倍（k=2*k）
+        dummyHead = ListNode(-1, head)
+        k = 1
         while k < n:
-            # 依次合并两个长度为 k 的链表
-            pre, cur = dummy_head, dummy_head.next
-            while cur:
+            cur = dummyHead.next
+            pre = dummyHead
 
-                '''第一个长度为 k 的链表，以left为头节点'''
-                left = cur      
-                for i in range(1, k):    # k-1 步即可走到 left 链表末尾
+            while cur:
+                left = cur
+                for i in range(1, k):
                     if cur.next:
                         cur = cur.next
-                    else:       #  left链表长度未达到k
+                    else:
                         break
-                
-                # left链表长度未达到k也意味着后续right链表不存在，
-                # 因此无需合并left和right链表，直接将left附加到上一个链表结尾，提前结束即可
                 if not cur.next:
                     pre.next = left
                     break
-                
-                '''第二个长度为 k 的链表，以right为头节点'''
-                right = cur.next    
-                cur.next = None         # 断开left和right指代的链表
+
+                right = cur.next
+                cur.next = None
                 cur = right
-                for i in range(1, k):   # k-1 步即可走到 right 链表末尾
-                    if cur and cur.next:
+                for i in range(1, k):
+                    if cur.next:
                         cur = cur.next
-                    else:       #  right链表长度未达到k
+                    else:
                         break
                 
-                # 断开right链表与后续链表
                 nxt = None
                 if cur.next:
                     nxt = cur.next
                     cur.next = None
-                cur = nxt   # cur继续指向下一个链表（如存在）
+                cur = nxt
 
-                '''合并 left 和 right 两个链表'''
-                merged = mergeSort(left, right)
-                pre.next = merged
+                pre.next = mergeSort(left, right)
                 while pre.next:
-                    pre = pre.next  # pre 移动到当前排序好的链表尾部
-
-            k *= 2   # 子链表长度 x2
-        
-        return dummy_head.next
+                    pre = pre.next
+            k *= 2
+        return dummyHead.next
 
                
