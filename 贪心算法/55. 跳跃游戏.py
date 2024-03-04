9677dff5f80c8611006f826576bc8b20a326b4bb
Sea-173 <2534062529@qq.com>
2024-03-04 14:30:53 +0800

    0304

---
+++ 贪心算法/55. 跳跃游戏.py
@@ -0,0 +1,43 @@
+"""
+给你一个非负整数数组 nums ，你最初位于数组的 第一个下标 。数组中的每个元素代表你在该位置可以跳跃的最大长度。
+
+判断你是否能够到达最后一个下标，如果可以，返回 true ；否则，返回 false 。
+"""
+
+"""
+思路总结：
+    经典贪心算法，下面的解法有点笨，保存了整个数组。
+    好的解法应该是只保存当前所能到达的最远位置，然后每次更新这个位置，如果更新后的位置大于等于数组长度，则返回True。
+"""
+
+from typing import List
+
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        if len(nums) == 1:
+            return True
+
+        stack = [0 for _ in nums]
+        stack[0] = nums[0]
+        cur = 0
+
+        while cur < len(stack):
+            if stack[-1] == 1:
+                return True
+            if stack[cur] == 0:
+                cur += 1
+                continue
+
+            tmp = cur
+            for _ in range(1, nums[cur]+1):
+                if tmp >= len(stack):
+                    return True
+                tmp += 1
+                stack[tmp] = 1
+            cur += 1
+        return False
+        
+s1 = Solution()
+nums = [2,3,1,1,4]
+print(s1.canJump(nums))
