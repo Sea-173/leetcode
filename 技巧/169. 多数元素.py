694dc3c6069b00e60a28503409aee4f16d4c59f7
Sea-173 <2534062529@qq.com>
2024-03-12 15:21:40 +0800

    0312

---
+++ 技巧/169. 多数元素.py
@@ -0,0 +1,17 @@
+"""
+给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+
+你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+"""
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        votes = 0
+        for num in nums:
+            if votes == 0:
+                x = num
+            if num == x:
+                votes += 1
+            else:
+                votes -= 1
+        return x
